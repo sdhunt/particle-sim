216ab5a179b188ee3f54d1bec766ec25a93cb18d
Simon Hunt <sdhunt@gmail.com>
2019-04-14 18:33:56 -0700

    Initial Commit.

---
+++ src/main/java/com/meowster/psim/Counter.java
@@ -0,0 +1,19 @@
+public class Counter {
+  private final int max;
+
+  private int count;
+
+  public Counter(int max) {
+    this.max = max;
+    count = max;
+  }
+
+  public int count() {
+    count = count == 0 ? max : count - 1;
+    return count;
+  }
+
+  public boolean atZero() {
+    return count == 0;
+  }
+}
